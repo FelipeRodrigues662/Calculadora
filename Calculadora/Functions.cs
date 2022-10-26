ecb86b34d53b45fd89dfb07c6c677df0ec123902
feliperodrigues_frwk <feliperodrigues_frwk@MAGNATI-10845-F>
2022-10-26 14:09:58 -0300

    Adicionar arquivos de projeto.

---
+++ Calculadora/Functions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.Intrinsics;
+
+namespace Calculadora
+{
+    class Function {
+
+        private float valor1, valor2;
+
+        public void Retorno() 
+        {
+            Console.WriteLine("Digite o Valor 1:");
+            valor1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o Valor 2:");
+            valor2 = float.Parse(Console.ReadLine());
+        }
+
+        public void Soma()
+        {
+            Console.WriteLine($"{valor1}+{valor2} = {valor1+valor2}\n");
+        }
+
+        public void Subtracao()
+        {
+            Console.WriteLine($"{valor1}-{valor2} = {valor1 - valor2}\n");
+        }
+
+        public void Divisao()
+        {
+            Console.WriteLine($"{valor1}/{valor2} = {valor1 / valor2}\n");
+        }
+
+        public void Multiplicacao()
+        {
+            Console.WriteLine($"{valor1}X{valor2} = {valor1 * valor2}\n");
+        }
+    }
+}
---
+++ Calculadora/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class App
+    {
+        static void Main()
+        {
+            
+            int cod;
+            var func = new Function();
+
+            do
+            {
+                Console.WriteLine("|------------------------|------------------------------|");
+                Console.WriteLine("|        Funções         |           Código             |");
+                Console.WriteLine("|------------------------|------------------------------|");
+                Console.WriteLine("|          Soma          |              1               |");
+                Console.WriteLine("|        Subtração       |              2               |");
+                Console.WriteLine("|         Divisão        |              3               |");
+                Console.WriteLine("|      Multiplicação     |              4               |");
+                Console.WriteLine("|          Sair          |              5               |");
+                Console.WriteLine("|------------------------|------------------------------|");
+
+                Console.WriteLine("\nDigite o código:");
+                cod = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+                switch (cod)
+                {
+                    case 1:
+                        func.Retorno();
+                        Console.Clear();
+                        func.Soma();                        
+                    break;
+
+                    case 2:
+                        func.Retorno();
+                        Console.Clear();
+                        func.Subtracao();                      
+                    break;
+
+                    case 3:
+                        func.Retorno();
+                        Console.Clear();
+                        func.Divisao();                       
+                    break;
+
+                    case 4:
+                        func.Retorno();
+                        Console.Clear();
+                        func.Multiplicacao();
+                        break;
+
+                    case 5:
+                        Console.WriteLine("O Programa irá fechar!!!");
+                    break;
+                    default:
+                        Console.WriteLine("Código Inválido");
+                    break;
+                }
+            } while (cod != 5);
+        }
+    }
+}
